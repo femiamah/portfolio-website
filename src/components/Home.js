564e025f2c109b62124487f5bebbaa5d99b8b4ca
Michael Amah <femiamah@gmail.com>
2024-08-09 09:43:43 +0100

    Added Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section id="home">
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is the Home section of my portfolio website.</p>
+        </section>
+    );
+};
+
+export default Home;
